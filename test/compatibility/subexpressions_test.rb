fa8e82c79b2f1a3d4528228ec43a513a3a501b0c
Matijs van Zuijlen <matijs@matijs.net>
2025-07-12 23:22:21 +0200

    Add compatibility test for subexpressions

---
+++ test/compatibility/subexpressions_test.rb
@@ -0,0 +1,241 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+# Based on spec/subexpressions.js in handlebars.js. The content of the specs should
+# mostly be identical to the content there, so a side-by-side diff should show
+# spec equivalence, and show any new specs that should be added.
+#
+# spec/subexpressions.js in handlebars.js is covered by the MIT license. See README.md
+# for details.
+
+describe 'subexpressions' do
+  it 'arg-less helper' do
+    skip
+    expectTemplate('{{foo (bar)}}!')
+      .withHelpers({
+        foo: lambda { |val|
+          return val + val;
+        },
+        bar: lambda {
+          return 'LOL';
+        },
+      })
+      .toCompileTo('LOLLOL!');
+  end
+
+  it 'helper w args' do
+    skip
+    expectTemplate('{{blog (equal a b)}}')
+      .withInput({ bar: 'LOL' })
+      .withHelpers({
+        blog: lambda { |val|
+          return 'val is ' + val;
+        },
+        equal: lambda { |x, y|
+          return x == y;
+        },
+      })
+      .toCompileTo('val is true');
+  end
+
+  it 'mixed paths and helpers' do
+    skip
+    expectTemplate('{{blog baz.bat (equal a b) baz.bar}}')
+      .withInput({ bar: 'LOL', baz: { bat: 'foo!', bar: 'bar!' } })
+      .withHelpers({
+        blog: lambda { |val, that, theOther|
+          return 'val is ' + val + ', ' + that + ' and ' + theOther;
+        },
+        equal: lambda { |x, y|
+          return x == y;
+        },
+      })
+      .toCompileTo('val is foo!, true and bar!');
+  end
+
+  it 'supports much nesting' do
+    skip
+    expectTemplate('{{blog (equal (equal true true) true)}}')
+      .withInput({ bar: 'LOL' })
+      .withHelpers({
+        blog: lambda { |val|
+          return 'val is ' + val;
+        },
+        equal: lambda { |x, y|
+          return x == y;
+        },
+      })
+      .toCompileTo('val is true');
+  end
+
+  it 'GH-800 : Complex subexpressions' do
+    skip
+    context = { a: 'a', b: 'b', c: { c: 'c' }, d: 'd', e: { e: 'e' } };
+    helpers = {
+      dash: lambda { |a, b|
+        return a + '-' + b;
+      },
+      concat: lambda { |a, b|
+        return a + b;
+      },
+    };
+
+    expectTemplate("{{dash 'abc' (concat a b)}}")
+      .withInput(context)
+      .withHelpers(helpers)
+      .toCompileTo('abc-ab');
+
+    expectTemplate('{{dash d (concat a b)}}')
+      .withInput(context)
+      .withHelpers(helpers)
+      .toCompileTo('d-ab');
+
+    expectTemplate('{{dash c.c (concat a b)}}')
+      .withInput(context)
+      .withHelpers(helpers)
+      .toCompileTo('c-ab');
+
+    expectTemplate('{{dash (concat a b) c.c}}')
+      .withInput(context)
+      .withHelpers(helpers)
+      .toCompileTo('ab-c');
+
+    expectTemplate('{{dash (concat a e.e) c.c}}')
+      .withInput(context)
+      .withHelpers(helpers)
+      .toCompileTo('ae-c');
+  end
+
+  it 'provides each nested helper invocation its own options hash' do
+    skip
+    lastOptions = null;
+    helpers = {
+      equal: lambda { |x, y, options|
+        if !options || options == lastOptions
+          throw new Error('options hash was reused');
+        end
+        lastOptions = options;
+        return x == y;
+      },
+    };
+    expectTemplate('{{equal (equal true true) true}}')
+      .withHelpers(helpers)
+      .toCompileTo('true');
+  end
+
+  it 'with hashes' do
+    skip
+    expectTemplate("{{blog (equal (equal true true) true fun='yes')}}")
+      .withInput({ bar: 'LOL' })
+      .withHelpers({
+        blog: lambda { |val|
+          return 'val is ' + val;
+        },
+        equal: lambda { |x, y|
+          return x == y;
+        },
+      })
+      .toCompileTo('val is true');
+  end
+
+  it 'as hashes' do
+    skip
+    expectTemplate("{{blog fun=(equal (blog fun=1) 'val is 1')}}")
+      .withHelpers({
+        blog: lambda { |options|
+          return 'val is ' + options.hash.fun;
+        },
+        equal: lambda { |x, y|
+          return x == y;
+        },
+      })
+      .toCompileTo('val is true');
+  end
+
+  it 'multiple subexpressions in a hash' do
+    skip
+    expectTemplate(
+      '{{input aria-label=(t "Name") placeholder=(t "Example User")}}'
+    )
+      .withHelpers({
+        input: lambda { |options|
+          hash = options.hash;
+          ariaLabel = Handlebars.Utils.escapeExpression(hash['aria-label']);
+          placeholder = Handlebars.Utils.escapeExpression(hash.placeholder);
+          return new Handlebars.SafeString(
+            '<input aria-label="' +
+              ariaLabel +
+              '" placeholder="' +
+              placeholder +
+              '" />'
+          );
+        },
+        t: lambda { |defaultString|
+          return new Handlebars.SafeString(defaultString);
+        },
+      })
+      .toCompileTo('<input aria-label="Name" placeholder="Example User" />');
+  end
+
+  it 'multiple subexpressions in a hash with context' do
+    skip
+    expectTemplate(
+      '{{input aria-label=(t item.field) placeholder=(t item.placeholder)}}'
+    )
+      .withInput({
+        item: {
+          field: 'Name',
+          placeholder: 'Example User',
+        },
+      })
+      .withHelpers({
+        input: lambda { |options|
+          hash = options.hash;
+          ariaLabel = Handlebars.Utils.escapeExpression(hash['aria-label']);
+          placeholder = Handlebars.Utils.escapeExpression(hash.placeholder);
+          return new Handlebars.SafeString(
+            '<input aria-label="' +
+              ariaLabel +
+              '" placeholder="' +
+              placeholder +
+              '" />'
+          );
+        },
+        t: lambda { |defaultString|
+          return new Handlebars.SafeString(defaultString);
+        },
+      })
+      .toCompileTo('<input aria-label="Name" placeholder="Example User" />');
+  end
+
+  it 'subexpression functions on the context' do
+    skip
+    expectTemplate('{{foo (bar)}}!')
+      .withInput({
+        bar: lambda {
+          return 'LOL';
+        },
+      })
+      .withHelpers({
+        foo: lambda { |val|
+          return val + val;
+        },
+      })
+      .toCompileTo('LOLLOL!');
+  end
+
+  it "subexpressions can't just be property lookups" do
+    skip
+    expectTemplate('{{foo (bar)}}!')
+      .withInput({
+        bar: 'LOL',
+      })
+      .withHelpers({
+        foo: lambda { |val|
+          return val + val;
+        },
+      })
+      .toThrow();
+  end
+end
