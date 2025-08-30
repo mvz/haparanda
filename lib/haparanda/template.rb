4571ef740331ac5cf387c09040c19c30aad263a6
Matijs van Zuijlen <matijs@matijs.net>
2025-08-30 10:13:54 +0200

    Add support for the explicit_partial_context compile-time option

--- lib/haparanda/template.rb
+++ lib/haparanda/template.rb
@@ -11,19 +11,22 @@ module Haparanda
       @compile_options = compile_options
     end
 
-    def call(input, helpers: {}, **runtime_options)
+    def call(input, helpers: {}, data: {})
       all_helpers = @helpers.merge(helpers)
       if @compile_options[:known_helpers_only]
         keys = @compile_options[:known_helpers]&.keys || []
         all_helpers = all_helpers.slice(*keys)
       end
+      explicit_partial_context = true if @compile_options[:explicit_partial_context]
       # TODO: Change interface of HandlebarsProcessor so it can be instantiated
       # in Template#initialize
-      processor = HandlebarsProcessor.new(input,
-                                          helpers: all_helpers,
-                                          partials: @partials,
-                                          log: @log,
-                                          **runtime_options)
+      processor =
+        HandlebarsProcessor.new(input,
+                                helpers: all_helpers,
+                                partials: @partials,
+                                log: @log,
+                                data: data,
+                                explicit_partial_context: explicit_partial_context)
       processor.apply(@expr)
     end
   end
